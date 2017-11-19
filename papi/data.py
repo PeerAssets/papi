daa0ec557b6a83852e8116ca675c5f1895a102e2
saeveritt <saeveritt@gmail.com>
2017-11-19 16:27:26 -0600

    modify and support card listing

--- papi/data.py
+++ papi/data.py
@@ -3,6 +3,7 @@ from pypeerassets.paproto_pb2 import DeckSpawn, CardTransfer
 from models import Deck, Card, db
 from sqlalchemy.exc import IntegrityError
 from conf import *
+import sys
 
 node = pa.RpcNode(testnet=testnet)
 
@@ -24,7 +25,7 @@ def init_p2thkeys():
     return print('{} P2TH Key(s) Registered'.format(n))
 
 def init_decks():
-    import sys
+    from itertools import tee
     n = 0
 
     def message(n):
@@ -35,8 +36,8 @@ def init_decks():
         entry = db.session.query(Deck).filter(Deck.id == deck.id).first()
         subscribe = deck.id in subscribed     
         if not entry:
-            D = Deck( deck.id, deck.name, deck.issuer, pa.pautils.issue_mode_to_enum(DeckSpawn(), deck.issue_mode), deck.number_of_decimals, subscribe )
             try:
+                D = Deck( deck.id, deck.name, deck.issuer, pa.pautils.issue_mode_to_enum(DeckSpawn(), deck.issue_mode), deck.number_of_decimals, subscribe )
                 db.session.add(D)
                 db.session.commit()
             except IntegrityError as e:
@@ -47,35 +48,28 @@ def init_decks():
             db.session.commit()
 
     def add_cards(cards):
-        n = 0
-        if cards is None:
-            return
-        for card in cards:
-            card_id = card.txid + str(card.blockseq) + str(card.cardseq)
-            n += 1
-            entry = db.session.query(Card).filter(Card.id == card_id).first()   
-            if not entry:
-                C = Card( card_id, card.txid, card.cardseq, card.receiver[0], card.sender, card.amount[0], card.type, card.blocknum, card.blockseq, card.deck_id )
-                try:
+        for cardset in cards:
+            for card in cardset:
+                sys.stdout.write(card.txid +'\r')
+                sys.stdout.flush()
+                card_id = card.txid + str(card.blockseq) + str(card.cardseq)
+                entry = db.session.query(Card).filter(Card.id == card_id).first()   
+                if not entry:
+                    C = Card( card_id, card.txid, card.cardseq, card.receiver[0], card.sender, card.amount[0], card.type, card.blocknum, card.blockseq, card.deck_id )
                     db.session.add(C)
-                    db.session.commit()
-                except (IntegrityError, Exception) as e:
-                    print(e)
-                    pass
-
+                db.session.commit()
+        sys.stdout.flush()
 
     if not autoload:
         decks = [pa.find_deck(node,txid,version)[0] for txid in subscribed]
 
         for deck in decks:
             add_deck(deck)
-            if deck.id in subscribed:
-                add_cards( pa.find_card_transfers(node, deck) )
+            add_cards( pa.find_card_transfers(node, deck) )
             n += 1
             message(n)
 
     else:
-        from itertools import tee
         decks = pa.find_all_valid_decks(node, 1 , True)
         decks, _decks = tee(decks,2)
         while True:
@@ -89,15 +83,12 @@ def init_decks():
             except StopIteration:
                 break
 
-#def init_cards():
-#    for deck_id in subscribed:
-
 def init_pa():
     init_p2thkeys()
     init_decks()
-    #init_cards()
-    print('PeerAssets version {} Initialized'.format(version))
 
+    sys.stdout.write('PeerAssets version {} Initialized'.format(version))
+    sys.stdout.flush()
 
 
         
