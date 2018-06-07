c285b18f5024c7074c9cd83ac732b1272bbff7a0
Peerchemist <dx.1178@gmail.com>
2018-06-07 18:09:55 +0200

    separate dockerfile for the peercoind with transactions.py included

---
+++ peercoind/transaction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from requests import post
+from requests.exceptions import ConnectionError
+from sys import argv
+
+
+def wallet_notify(txid):
+
+    try:
+        post('http://0.0.0.0:5555/alert', data={'txid': txid})
+    except ConnectionError:
+        pass
+
+
+if __name__ == '__main__':
+    wallet_notify(argv[1])
